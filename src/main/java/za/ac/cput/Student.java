7aff13747791455d0e68ab3426d1064d6231ee25
Moipone <kthinasonke@gmail.com>
2019-03-07 14:10:27 +0200

    commits

--- src/main/java/za/ac/cput/Student.java
+++ src/main/java/za/ac/cput/Student.java
@@ -1,7 +1,9 @@
 package za.ac.cput;
 
 public class Student {
-public void names(String name, int StrudId, int Marks,String course, String lecturer){
-    return;
+
+public String names(String name, int StrudId, int Marks,String course, String lecturer){
+    String n = name + StrudId + Marks + course + lecturer;
+    return n;
 }
 }
---
+++ src/test/java/za/ac/cput/StudentTest.java
@@ -0,0 +1,21 @@
+package za.ac.cput;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    @Test
+    public void names() {
+    }
+}
